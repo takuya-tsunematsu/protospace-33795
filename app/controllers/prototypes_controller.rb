707445227e40cc72d134545a8b4007fe55f1c66c
takuya-tsunematsu <takuya.07.tsunematsu@gmail.com>
2021-04-23 12:08:03 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
-  before_action :authenticate_user!, exept: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit]
 
   def index
